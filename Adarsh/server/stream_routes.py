22a6360164769f53ba4de825c7a46b3609f7da0e
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-02-07 06:36:30 +0530

    Update stream_routes.py

--- Adarsh/server/stream_routes.py
+++ Adarsh/server/stream_routes.py
@@ -26,16 +26,11 @@ async def root_route_handler(_):
     return web.json_response(
         {
             "server_status": "running",
-            "uptime": get_readable_time(time.time() - StartTime),
             "telegram_bot": "@" + StreamBot.username,
             "connected_bots": len(multi_clients),
-            "loads": dict(
-                ("bot" + str(c + 1), l)
-                for c, (_, l) in enumerate(
-                    sorted(work_loads.items(), key=lambda x: x[1], reverse=True)
                 )
             ),
-            "version": __version__,
+            "Info" : "Go to Tg Bot Send files and enjoy hight speed streaming"
         }
     )
     
